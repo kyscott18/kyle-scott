179b5abeadbba9eaea16173eec99f99ec0461b3e
Kyle Scott <kyscott@umich.edu>
2024-01-13 11:33:09 -0500

    Refactor engine logic

--- src/Engine.sol
+++ src/Engine.sol
@@ -1,7 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import {getPairID, addLiquidity, removeLiquidity, swap, Strike} from "src/Pair.sol";
+import {
+    Strike,
+    getPairID,
+    swap as pairSwap,
+    addLiquidity as pairAddLiquidity,
+    removeLiquidity as pairRemoveLiquidity
+} from "src/Pair.sol";
+
+interface ICallback {
+    /// @param data Extra data passed back to the callback from the caller
+    function callback(bytes calldata data) external;
+}
 
 contract Engine {
     /*<//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\>
@@ -13,24 +24,6 @@ contract Engine {
     /*<//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\>
                                DATA TYPES
     <//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\>*/
-
-    enum Commands {
-        Swap,
-        WrapWETH,
-        UnwrapWETH,
-        AddLiquidity,
-        RemoveLiquidity
-    }
-
-    enum TokenSelector {
-        Token0,
-        Token1
-    }
-
-    struct CommandInput {
-        Commands command;
-        bytes input;
-    }
 
     struct SwapParams {
         address token0;
@@ -56,63 +49,52 @@ contract Engine {
                                 STORAGE
     <//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\>*/
 
-    address payable immutable weth;
-
     mapping(bytes32 pairID => mapping(uint256 strike => bytes32)) public strikeHashes;
-
-    /*<//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\>
-                              CONSTRUCTOR
-    <//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\>*/
-
-    constructor(address payable _weth) {
-        weth = _weth;
-    }
 
     /*<//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\>
                                  LOGIC
     <//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\>*/
 
-    function execute(CommandInput[] calldata commandInputs) external {
-        unchecked {
-            for (uint256 i = 0; i < commandInputs.length; i++) {
-                if (commandInputs[i].command == Commands.Swap) {
-                    SwapParams memory params = abi.decode(commandInputs[i].input, (SwapParams));
-                    bytes32 pairID = getPairID(params.token0, params.token1);
+    function swap(SwapParams memory params, bytes calldata data) external {
+        bytes32 pairID = getPairID(params.token0, params.token1);
 
-                    for (uint256 j = 0; j < params.strikes.length; j++) {
-                        bytes32 strikeHash = keccak256(abi.encode(params.strikes[j]));
-                        if (strikeHashes[pairID][params.strikes[j].ratio] != strikeHash) revert InvalidStrikeHash();
+        for (uint256 i = 0; i < params.strikes.length; i++) {
+            Strike memory strike = params.strikes[i];
 
-                        swap(params.strikes[j]);
+            bytes32 strikeHash = keccak256(abi.encode(strike));
+            if (strikeHashes[pairID][strike.ratio] != strikeHash) revert InvalidStrikeHash();
 
-                        strikeHashes[pairID][params.strikes[j].ratio] = keccak256(abi.encode(params.strikes[j]));
-                    }
-                } else if (commandInputs[i].command == Commands.WrapWETH) {
-                    //
-                } else if (commandInputs[i].command == Commands.UnwrapWETH) {
-                    //
-                } else if (commandInputs[i].command == Commands.AddLiquidity) {
-                    AddLiquidityParams memory params = abi.decode(commandInputs[i].input, (AddLiquidityParams));
-                    bytes32 pairID = getPairID(params.token0, params.token1);
+            pairSwap(strike);
 
-                    bytes32 strikeHash = keccak256(abi.encode(params.strike));
-                    if (strikeHashes[pairID][params.strike.ratio] != strikeHash) revert InvalidStrikeHash();
-
-                    addLiquidity(params.strike, params.amount);
-
-                    strikeHashes[pairID][params.strike.ratio] = keccak256(abi.encode(params.strike));
-                } else if (commandInputs[i].command == Commands.RemoveLiquidity) {
-                    RemoveLiquidityParams memory params = abi.decode(commandInputs[i].input, (RemoveLiquidityParams));
-                    bytes32 pairID = getPairID(params.token0, params.token1);
-
-                    bytes32 strikeHash = keccak256(abi.encode(params.strike));
-                    if (strikeHashes[pairID][params.strike.ratio] != strikeHash) revert InvalidStrikeHash();
-
-                    removeLiquidity(params.strike, params.amount);
-
-                    strikeHashes[pairID][params.strike.ratio] = keccak256(abi.encode(params.strike));
-                }
-            }
+            strikeHashes[pairID][strike.ratio] = keccak256(abi.encode(strike));
         }
+
+        ICallback(msg.sender).callback(data);
+    }
+
+    function addLiquidity(AddLiquidityParams memory params, bytes calldata data) external {
+        bytes32 pairID = getPairID(params.token0, params.token1);
+
+        bytes32 strikeHash = keccak256(abi.encode(params.strike));
+        if (strikeHashes[pairID][params.strike.ratio] != strikeHash) revert InvalidStrikeHash();
+
+        pairAddLiquidity(params.strike, params.amount);
+
+        strikeHashes[pairID][params.strike.ratio] = keccak256(abi.encode(params.strike));
+
+        ICallback(msg.sender).callback(data);
+    }
+
+    function removeLiquidity(RemoveLiquidityParams memory params, bytes calldata data) external {
+        bytes32 pairID = getPairID(params.token0, params.token1);
+
+        bytes32 strikeHash = keccak256(abi.encode(params.strike));
+        if (strikeHashes[pairID][params.strike.ratio] != strikeHash) revert InvalidStrikeHash();
+
+        pairRemoveLiquidity(params.strike, params.amount);
+
+        strikeHashes[pairID][params.strike.ratio] = keccak256(abi.encode(params.strike));
+
+        ICallback(msg.sender).callback(data);
     }
 }
